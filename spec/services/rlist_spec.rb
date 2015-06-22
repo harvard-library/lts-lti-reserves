a111b805dfe66ccd9d74f1839008e4d59a836255
bobbi-SMR <bobbi_fox@harvard.edu>
2015-06-22 14:02:38 +0000

    rewritten to match current rspec conventions ("expect")

--- spec/services/rlist_spec.rb
+++ spec/services/rlist_spec.rb
@@ -6,7 +6,7 @@ describe Rlist do
       WebMock.allow_net_connect!
       rlist = Rlist.new
       response = rlist.list('78419')
-      assert_equal(response.code, 200)
+      expect(response.code).to eq(200)
       WebMock.disable_net_connect!
     end
   end
