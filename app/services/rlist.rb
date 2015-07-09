b9bd31a97afc5c4dd2d3154791aebb56c2611dd9
bobbi-SMR <bobbi_fox@harvard.edu>
2015-07-09 18:43:37 +0000

    slightly better error handling

--- app/services/rlist.rb
+++ app/services/rlist.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+
 #require 'dotenv'
 #Dotenv.load
 class Rlist
@@ -8,14 +9,25 @@ class Rlist
   def list(course_id)
     response = self.class.get("/course/" + course_id.to_s,
                             :headers => {"User-Agent" => "lts-lti-reserves"} )
-    raise RuntimeError.new("Unsuccessful  call to " + self.base_uri.to_s + " for course (" + course_id.to_s + ") with code " + response.code.to_s) if response.code != 200
+    if response.code != 200
+      msg = "Unsuccessful  call for course ( #{course_id}). "
+      if response
+        err = MultiXml.parse(response.body)
+        msg = "#{msg} #{err["error"]["status"]}"
+      else
+        msg = "#{response.code}: #{response.message}"
+      end
+      raise RuntimeError.new(msg);
+    end
+ #  raise ApiError.new(response.code, response.message), "Unable to get list for course (#{course_id})" if response.code != 200
     response
   end
 end
 
 #rlist = Rlist.new
 
-#response =  rlist.list('35109900000')
+#response =  rlist.list('351089')
+#response =  rlist.list('35108900000000000000')
 #puts response.code
 #puts "body"
 #puts response.body
