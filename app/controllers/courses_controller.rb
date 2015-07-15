728ac1fdf97ead55479cb4b6f672abf91584fed2
bobbi-SMR <bobbi_fox@harvard.edu>
2015-07-15 20:34:48 +0000

    moved where flash happens, deletion working.

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -22,16 +22,16 @@ class CoursesController < ApplicationController
       params[:res_ids].each do |res_id|
 # TBD: handling errors!
         ret_str = delete_one(params[:id], res_id)
-        if ret_str
-          err_str = "#{err_str}; #{ret_str}"
-        else
+        if ret_str.empty?
           count = count + 1 
+        else
+          err_str = "#{err_str}; #{ret_str}"
         end
       end
     end
     @reserves = Course.new(params[:id]).list
     flash[:notice] = "#{count} reserve#{'s' if count != 1} deleted"
-    flash[:error] = err_str if err_str
+    flash[:error] = err_str if !err_str.empty?
     redirect_to :action => :show, id: params[:id]
   end
 end
