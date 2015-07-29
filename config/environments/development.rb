db822e2417a6fc1133ce1ddc450d81026f92685a
bobbi-SMR <bobbi_fox@harvard.edu>
2015-07-29 11:43:27 -0400

    checkbox values reflect expected values, rather than 0 and 1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+#  BetterErrors::Middleware.allow_ip!(ENV['B_E_ALLOWED'])
+
+  # allows Better Errors to talk to my browser
 end
