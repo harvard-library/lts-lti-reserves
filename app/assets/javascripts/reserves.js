2fd5b44d0dfaca9ae43c5bfbd4ff7c516a3c720c
bobbi-SMR <bobbi_fox@harvard.edu>
2015-07-30 14:21:28 -0400

    more work on styling, begin work on javascripting.  submit stoppage not happening :-(

---
+++ app/assets/javascripts/reserves.js
@@ -0,0 +1,47 @@
+/* set up a jquery object with the datepicker. if isotarget defined, 
+   add the change date event handler for ISO string conversion
+*/
+  var updates = ["estimated_enrollment", "student_annotation", "lecture_date", "required", "visibility"];
+
+ $(document).on("ready page:load", function(e) {
+                 if ($("body").hasClass("c_reserves")) {
+		    if ($("body").hasClass("a_edit")) {
+		       setupDatepicker($("#reserve_lecture_date"), $("#iso_date"));
+			$("#edit_reserve").submit(function() { 
+			    submitUpdate(e);
+			});
+		    }
+		 }
+});
+  function submitUpdate(e) {
+      e.preventDefault();
+      var change = false;
+      for ( i = 0; i < updates.length; i++) {
+	    if ($("#o_"+updates[i]).val() !== $("#reserve_" + updates[i]).val()) {
+		change = true;
+		break;
+	  }
+      }
+      if (!change) {
+	  alert("No changes have been made");
+	  return false;
+      } else {
+	  $(this).submit();
+      }
+}
+  function setupDatepicker($which, $isotarget) {
+	 $which.datepicker({
+                       startDate: "now",
+                       endDate: "+365d",
+                       clearBtn: true,
+                       todayHighlight: true,
+                       toggleActive: true,
+                       orientation: "top auto"
+	});
+	if (typeof $isotarget == "object") {
+	   $which.datepicker().on("changeDate", function(e) {
+	      $isotarget.val(e.date.toISOString());
+	   });
+	}
+  }
+
