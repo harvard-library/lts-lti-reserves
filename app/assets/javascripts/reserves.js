4a29c8e618dbb5bd5ad38692effc5fa92c5fa916
bobbi-SMR <bobbi_fox@harvard.edu>
2015-08-06 16:34:03 -0400

    starting to add the javascript functionality

--- app/assets/javascripts/reserves.js
+++ app/assets/javascripts/reserves.js
@@ -5,11 +5,67 @@
 
  $(document).on("ready page:load", function(e) {
                  if ($("body").hasClass("c_reserves")) {
-		    if ($("body").hasClass("a_edit")) {
+		    if ($("body").hasClass("a_edit") ) {
 		       setupDatepicker($("#reserve_lecture_date"), $("#iso_date"));
+		    }
+		    else if ( $("body").hasClass("a_new")) {
+                       setupDatepicker($("#reserve_lecture_date"), $("#iso_date"));
+			material_type_change($("#reserve_input_material_type")); /* when we come back with an error */
+			$("#reserve_input_material_type").on("change", function(e) {
+			    material_type_change($(this));
+			});
 		    }
 		 }
 });
+
+  /*   COMMON FUNCTIONS */
+
+  function setupDatepicker($which, $isotarget) {
+	 $which.datepicker({
+                       startDate: "now",
+                       endDate: "+365d",
+                       clearBtn: true,
+                       todayHighlight: true,
+                       toggleActive: true,
+                       orientation: "top auto"
+	});
+	if (typeof $isotarget == "object") {
+	   $which.datepicker().on("changeDate", function(e) {
+	      $isotarget.val(e.date.toISOString());
+	   });
+	}
+  }
+
+/* CREATION FUNCTION(S) */
+
+function material_type_change($this) {
+    var val = $this.val();
+    if (val === "") {
+	switch_type("");
+    } 
+    else {
+	switch_type(($(this).val() === "Journal Article")? "JOURNAL" : "NON_JOURNAL");
+    }
+    return;
+}
+
+/* shows and hides elements according to type */ 
+ function switch_type(type) {
+     $("#switchable").hide();
+     
+     if (type === "JOURNAL") {
+	 $(".nonjournal").hide();
+	 $(".journal").show();
+     }
+     else if (type === "NON_JOURNAL") {
+	 $(".nonjournal").show();
+	 $(".journal").hide();
+     }
+     else { return; }
+     $(".both").show();
+     $("#switchable").show();
+ }
+
   function submitUpdate(e) {
       e.preventDefault();
       var change = false;
@@ -26,19 +82,5 @@
 	  $(this).submit();
       }
 }
-  function setupDatepicker($which, $isotarget) {
-	 $which.datepicker({
-                       startDate: "now",
-                       endDate: "+365d",
-                       clearBtn: true,
-                       todayHighlight: true,
-                       toggleActive: true,
-                       orientation: "top auto"
-	});
-	if (typeof $isotarget == "object") {
-	   $which.datepicker().on("changeDate", function(e) {
-	      $isotarget.val(e.date.toISOString());
-	   });
-	}
-  }
+
 
