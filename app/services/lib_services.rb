07be0eb6ebf584c41378f79a7cf22788ecba3ad3
bobbi-SMR <bobbi_fox@harvard.edu>
2015-08-11 11:07:09 -0400

    refactored to return a usable hash

--- app/services/lib_services.rb
+++ app/services/lib_services.rb
@@ -7,12 +7,19 @@ class LibServices
 
   def journal_cite(id)
     jid = CGI.escape(id)
-    fetch_cite("/journal/#{jid}")
+    response = fetch_cite("/journal/#{jid}")
+    json = ""
+    json = JSON.parse(response.body)['rlistFormat']['journalArticle'] if response.code == 200
+    cite_from_json(response.code,json)
   end
 
   def hollis_cite(id)
-    fetch_cite("/hollis/#{id}")
+    response = fetch_cite("/hollis/#{id}")
+    json = ""
+    json = JSON.parse(response.body)['rlistFormat']['hollis'] if response.code == 200
+    cite_from_json(response.code, json)
   end
+
 
   # calls to both hollis and journal end up the same way, so why not say so? :-)
   def fetch_cite(fragment)
@@ -22,5 +29,44 @@ class LibServices
     handle_bad_response(response,"Problem getting HOLLIS cite for #{id}") if response.code != 200 && response.code != 404
     response
   end
-
+ 
+  # process the input json (if it exists, returning a "cite" hash that's ready to go!)
+  def cite_from_json(code, json)
+    cite = {}
+    cite["status"] = code
+    if code == 200
+      json.each do |k, v|
+        if k == "authors" 
+          if v["authorName"][0]
+            cite["author_first_name"] =  v["authorName"][0]["authorFirst"] || ""
+            cite["author_last_name"] =  v["authorName"][0]["authorLast"] || ""
+          end
+          if v["editorName"]
+            cite["editor_first_name"] = v["editorName"]["editorFirst"] || ""
+            cite["editor_last_name"] =  v["editorName"]["editorLast"] || ""
+          end
+        elsif k == "isbn"
+          cite["isbn"] = ""
+          if v.kind_of?(Array)
+            v.each { |i| cite["isbn"] = "#{cite["isbn"]} #{i},"}
+            cite["isbn"].chop!.strip!
+          else
+            cite["isbn"] = v
+          end
+        elsif k == "articleTitle"
+          cite["title"] = v
+        elsif k == "pubInfo"
+          cite["publisher"] = v
+        elsif k == "hollisId"
+          cite["hollis_system_number"] = v.to_s
+        elsif k == "pmc"
+          cite["pmc"] = v
+          cite["url"] = "http://www.ncbi.nlm.nih.gov/pmc/articles/#{v}"
+        else
+          cite[k.underscore] = v.to_s
+        end
+      end
+    end
+    cite #return cite
+  end
 end
--- spec/services/lib_services_spec.rb
+++ spec/services/lib_services_spec.rb
@@ -4,42 +4,55 @@ describe LibServices do
     it "successfully gets a hollis citation" do
       WebMock.allow_net_connect!
       libs = LibServices.new
-      response = libs.hollis_cite('003617028')
-puts response.body
-      cite = JSON.parse(response.body)['rlistFormat']['hollis']
-      expect(response.code).to eq(200)
-      expect(cite['hollisId'].start_with?('003617028')).to eq(true)
-      expect(cite['year'].to_s).to eq("1994")
+      cite = libs.hollis_cite('003617028')
+      expect(cite["status"]).to eq(200)
+      expect(cite['hollis_system_number'].start_with?('003617028')).to eq(true)
+      expect(cite['year']).to eq("1994")
+      expect(cite['title']).to eq("The Internet guide for new users")
       WebMock.disable_net_connect!
     end
     it "tries to get something that isn't there" do
       WebMock.allow_net_connect!
-      response = LibServices.new.hollis_cite('993617028')
-      expect(response.code).to eq(404)
+      cite = LibServices.new.hollis_cite('993617028')
+      expect(cite["status"]).to eq(404)
       WebMock.disable_net_connect!
+    end
+    it "tried to get something with an editor and author" do
+      WebMock.allow_net_connect!
+      cite = LibServices.new.hollis_cite('001254979')
+puts cite
+      expect(cite["status"]).to eq(200)
+      expect(cite['author_last_name']).to eq("Maimonides")
+      expect(cite['author_first_name']).to eq("Moses")
+      expect(cite['editor_last_name']).to eq("Twersky")
+      expect(cite['editor_first_name']).to eq("Isadore")
+      expect(cite['publisher']).to eq("New York: Behrman House")
+      expect(cite['isbn']).to eq("0874412064 (pbk.), 874412005")
     end
   end
   describe "journal" do
     it "successfully gets a doi" do
       WebMock.allow_net_connect!
       libs = LibServices.new
-      response = libs.journal_cite("10.1016/j.pedn.2008.07.009")
-      cite = JSON.parse(response.body)['rlistFormat']['journalArticle']
-      expect(response.code).to eq(200)
+      cite = libs.journal_cite("10.1016/j.pedn.2008.07.009")
+      expect(cite["status"]).to eq(200)
       expect(cite['doi']).to eq("10.1016/j.pedn.2008.07.009")
-      expect(cite['journalTitle']).to eq("Journal of Pediatric Nursing")
+      expect(cite['journal_title']).to eq("Journal of Pediatric Nursing")
       expect(cite['pubmed']).to be_nil
       WebMock.disable_net_connect!
     end
     it "successfully gets a PUBMED article" do
       WebMock.allow_net_connect!
-      response = LibServices.new.journal_cite("11079970")
-      cite = JSON.parse(response.body)['rlistFormat']['journalArticle']
-      expect(response.code).to eq(200)
+      cite = LibServices.new.journal_cite("11079970")
+puts cite
+      expect(cite["status"]).to eq(200)
       expect(cite['pubmed'].to_s).to eq("11079970")
       expect(cite['doi']).to be_nil
+      expect(cite['end_page']).to eq("684")
+      expect(cite['author_last_name']).to eq("Reilly")
+      expect(cite['title']).to eq("A knowledge-based patient assessment system: conceptual and technical design.")
+      expect(cite['url']).to eq("http://www.ncbi.nlm.nih.gov/pmc/articles/PMC2243964")
       WebMock.disable_net_connect!
     end
   end
- 
 end
