71313521abc830ca5e738d65b5d1e3fbfea5d3be
bobbi-SMR <bobbi_fox@harvard.edu>
2015-08-18 11:39:52 -0400

    fixed bugs in title setting

--- app/models/instance_info.rb
+++ app/models/instance_info.rb
@@ -6,18 +6,18 @@ InstanceInfo = Struct.new(:id, :course_id, :title, :catalog, :term, :primary, :x
     json = Icommons.new.course_instance(id)
     self.course_id = json["course"]["course_id"]
     compose_desc(json) # get title, catalog, term
-    self.primary =  json["primary_xlist_instances"].empty? ? nil :  json["primary_xlist_instances"][0].slice(/\d\d+/)
+    self.primary =  json["primary_xlist_instances"].empty? ? nil :  json["primary_xlist_instances"][0].slice(/\d\d+/).to_i
     get_xreg_ids(json["secondary_xlist_instances"])
   end
   def compose_desc(json)
     school = json["term"]["school_id"].upcase!
-    school = "FAS" if school == "COLGAS"
+    school = "FAS" if school == "COLGSAS"
     self.catalog = "#{school}-#{json['course']['registrar_code_display']}"
     self.term = json["term"]["display_name"]
     title = json["title"] 
     sub = json["sub_title"]
     self.title = title
-    self.title = "#{sub.title}: #{sub}" if !sub.blank? && title != sub
+    self.title = "#{self.title}: #{sub}" if !sub.blank? && title != sub
     short = json["short_title"]
     self.title = "[#{short}] #{self.title}" if short != title
   end
--- spec/models/instance_info_spec.rb
+++ spec/models/instance_info_spec.rb
@@ -2,16 +2,28 @@ require 'rails_helper'
 
 describe InstanceInfo do
   describe "create" do
-    it "successfully constructs itself" do
+    it "successfully constructs itself as a primary" do
       id = 356987
       WebMock.allow_net_connect!
       ii = InstanceInfo.new(id)
       WebMock.disable_net_connect!
       expect(ii.course_id).to eq(77231)
+      expect(ii.catalog).to eq("HLS-2470")
       expect(ii.title).to eq("Law and Philosophy Seminar")
       expect(ii.term).to eq("Fall 2015")
       expect(ii.primary).to be_nil
       expect(ii.xreg_ids.count).to eq(2)
+    end
+    it "successfully constructs itself as a secondary" do
+      id = 360017
+
+      WebMock.allow_net_connect!
+      ii = InstanceInfo.new(id)
+      WebMock.disable_net_connect!
+      expect(ii.course_id).to eq(76451)
+      expect(ii.catalog).to eq("FAS-113481")
+      expect(ii.title).to eq("PHIL 277: Law and Philosophy Colloquium")
+      expect(ii.primary).to eq(356987)
     end
   end
 end
