b7393b280860bb939dc5f02071869265d6c17e1e
bobbi-SMR <bobbi_fox@harvard.edu>
2015-08-27 15:37:11 -0400

    support re-use

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -38,14 +38,50 @@ class CoursesController < ApplicationController
 #    flash[:error] = "Unable to retrieve information from iCommons on Course Instance #{params[:id]}: #{bang}"
   end
   def reuse
-# here's where all the re-usability magic happens!
+    # here's where all the re-usability magic happens!
+    count = 0
+    err_str = ""
+    opts = {}
+    opts["submittingSystem"] = "CANVAS"
+    opts["estimatedEnrollment"] ="0"
+    opts["libraryCode"] = params[:library_code]
+    opts["contactInstructorId"] = params[:contact_instructor_id] || ENV['HUID'] # we'll get this when we hook up with LTI
+    opts["instanceId"] = params[:id]
+    if params[:cit_ids]
+      params[:cit_ids].each do |cit_id|
+        ret_str = reuse_one(opts, cit_id)
+        if ret_str.empty?
+          count = count + 1
+        else
+          err_str = "#{err_str}; #{ret_str}"
+        end
+      end
+    end
+    @reserves = Course.new(params[:id]).list
+    flash[:notice] = "#{count} reserve#{'s' if count != 1} re-used"
+    flash[:error] = err_str if !err_str.empty?
+    redirect_to :action => :show, id: params[:id]
+  end
+  def reuse_one(opts, cit_id) 
+    begin
+      opts["citationId"] = cit_id
+      @reserve = Reserve.new(opts)
+      if !@reserve.valid?
+        @reserve.errors[:base] if @reserve.errors[:base]
+      else
+       resp = Rlist.new.create(opts["instanceId"],options)
+       ""
+      end
+    end
+  rescue StandardError  => bang
+    "Unable to create new Reserve from citation id #{cit_id} : #{bang}"
   end
   def delete_one(id, res_id)
     begin
       Rlist.new.delete(params[:id], res_id)
       ""
     end
-    rescue Exception => bang
+    rescue StandardError => bang
     "#Reserve ID #{res_id} failed: #{bang}; "
   end
   def delete
