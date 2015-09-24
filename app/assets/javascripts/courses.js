e04cb9462d7ad9917ae3d446f2f776cafa7b310a
bobbi-SMR <bobbi_fox@harvard.edu>
2015-09-24 17:12:00 -0400

    sort and search

--- app/assets/javascripts/courses.js
+++ app/assets/javascripts/courses.js
@@ -46,6 +46,18 @@ function anyChecked(type) {
     return retVal;
 }
 
+/* javascripting supporting re-ordering of reserves */
+function saveOrder() {
+    var data = $("#ids_del li").map(function() { 
+	return $(this).attr('id'); })
+    .get();
+    $("#sort_order").val(data.join("|"));
+}
+
+function submit_reorder() { 
+/*    console.log($("#form_reorder")); */
+   $("#form_reorder").submit();
+}
 
  $(document).on("ready page:load", function(e) {
      /* edit stuff */
