19066af67a8158825b8546639c866d9ddc4d649f
bobbi-SMR <bobbi_fox@harvard.edu>
2015-10-29 10:54:58 -0400

    add method for roles checking

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  # list of Canvas roles that allow user to be  editing
+  CAN_EDIT = ["Instructor", "urn:lti:role:ims/lis/TeachingAssistant", "ContentDeveloper", "urn:lti:instrole:ims/lis/Administrator"]
+  def can_edit?(current_user)
+    CAN_EDIT.each {|r| return true if current_user.has_role?(r) }
+    false;
+  end
 end
