5d91f36f00210b4283e98fc0779d9202b3ccb814
bobbi-SMR <bobbi_fox@harvard.edu>
2015-11-25 11:49:49 -0500

    Add restart to deploy tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,6 +45,15 @@ namespace :deploy do
     end
   end
 
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, :restart
+
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
