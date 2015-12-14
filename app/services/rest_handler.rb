32117295670dc6d6f2025bd935f3d0b9de4c416f
bobbi-SMR <bobbi_fox@harvard.edu>
2015-12-14 08:33:17 -0500

    handle non 200 better

--- app/services/rest_handler.rb
+++ app/services/rest_handler.rb
@@ -2,9 +2,12 @@ module RestHandler
 # right now, this only handles bad responses, but who knows what the future brings :-)
   def handle_bad_response(response, msg)
     if response && msg && response.body
-      err = MultiXml.parse(response.body)
-      msg = "#{msg} #{err["error"]["status"]} #{err["error"]["message"]}"
-#      msg = "#{msg} #{response.body}"
+      begin
+        err = MultiXml.parse(response.body)
+        msg = "#{msg} #{err["error"]["status"]} #{err["error"]["message"]}"
+      rescue
+        msg = "#{msg} #{response.code}: #{response.message}"
+      end
     else
       msg = "#{msg} #{response.code}: #{response.message}"
     end
