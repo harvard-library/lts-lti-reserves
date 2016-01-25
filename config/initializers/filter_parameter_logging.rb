762b9f50ffb641917fcc36b0a7f533dc416be5e8
bobbi-SMR <bobbi_fox@harvard.edu>
2016-01-25 09:54:09 -0500

    filter out CI

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password]
+Rails.application.config.filter_parameters += [:password, :oauth_consumer_key,:custom_canvas_user_login_id, :lis_person_contact_email_primary, :lis_person_name_family, :lis_person_name_full, :lis_person_name_given, :lis_person_sourcedid ]
