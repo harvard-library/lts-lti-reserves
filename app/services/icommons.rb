af0b43e30c9cc276532a18e4e56deba750a67b8d
bobbi-SMR <bobbi_fox@harvard.edu>
2017-09-11 15:02:46 -0400

    more debuggin

--- app/services/icommons.rb
+++ app/services/icommons.rb
@@ -8,12 +8,12 @@ class Icommons
   @@headers = {"User-Agent" => "lts-lti-reserves", 
     "Authorization" => @@auth, 
     "Accept" => "application/json"}
-  logger = ActiveSupport::TaggedLogging.new(Rails.logger)
+  @@logger = ActiveSupport::TaggedLogging.new(Rails.logger)
 
   def course_instance(cid)
     loc = !cid.to_s.match(/^\d+$/)  ? cid : "/course_instances/#{cid}/" 
-    logger.tagged("FOX") {
-      logger.warn {"course_instance: #{cid}, loc: #{loc}" }
+    @@logger.tagged("FOX") {
+      @@logger.warn {"course_instance: #{cid}, loc: #{loc}" }
     }
     response = self.class.get(loc,
                               :headers => @@headers)
@@ -22,8 +22,8 @@ class Icommons
   end
   def instances_from_course(id)
     loc = !id.to_s.match(/^\d+$/) ? id : "/courses/#{id}/" 
-    logger.tagged("FOX") {
-      logger.warn {"course: #{id}, loc: #{loc}" }
+    @@logger.tagged("FOX") {
+      @@logger.warn {"course: #{id}, loc: #{loc}" }
     }
     response = self.class.get(loc,
                               :headers => @@headers)
